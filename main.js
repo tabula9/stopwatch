5979dbb440595fec9c3672a5507390daa71b205d
EC2 Default User <ec2-user@ip-172-31-41-114.ap-southeast-2.compute.internal>
2023-05-02 21:28:54 +0000

    実践課題javascript1

---
+++ main.js
@@ -0,0 +1,63 @@
+// function clickButton() {
+
+//   let now = new Date();
+//   let seconds = now.getSeconds(); //秒を取得
+  
+//   if (seconds<10) {
+//     seconds = '0' + seconds;
+//   }
+  
+//   let minutes = now.getMinutes();
+//   if (minutes>9) {
+//     minutes = 9;
+//   }
+  
+//   let milliSeconds = now.getMilliseconds(); //ミリ秒を取得
+//   milliSeconds = milliSeconds/100;
+//   milliSeconds = Math.trunc(milliSeconds); //実数を整数にする
+  
+  
+//   document.getElementById('timer').innerHTML = minutes + ':' + seconds + ':' + milliSeconds;
+  
+// }
+let start;
+let interval;
+let kaishi = document.getElementById('kaishi');
+let timer = document.getElementById('timer');
+
+function clickButton() {
+  start = new Date();
+    
+  interval = setInterval(goTimer, 10); //指定した間隔ごとに関数を呼び出す
+    
+  $('#kaishi').prop('disabled',true);
+  $('#stop,#reset').prop('disabled',false);
+  } 
+  
+function stopButton() {
+  clearInterval(interval);
+  $('#stop').prop('disabled',true);
+  $('#kaishi').prop('disabled',false);
+  }
+
+function resetButton() {
+  timer.innerHTML ='0:0:0:0';
+  $('#reset').prop('disabled',true);
+}
+
+
+let goTimer = function() {
+  let now = new Date();
+  
+  let time = now.getTime() - start.getTime();
+  let milli = Math.trunc(time%1000/100);
+  let seconds = Math.trunc(time/1000);
+  let minutes = Math.trunc(seconds/60);
+  
+  seconds = seconds - minutes*60;
+  
+  let secondsLarge = Math.trunc(seconds/10);
+  let secondsSmall = seconds - secondsLarge*10;
+  
+  timer.innerHTML =minutes + ':' + secondsLarge + ':' + secondsSmall + ':' + milli;
+};
